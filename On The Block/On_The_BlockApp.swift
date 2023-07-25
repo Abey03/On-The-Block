711ac6f16ced29fb9d446ea66ad1413427cdafea
Abe Molina <abemolina03@gmail.com>
2023-07-24 22:46:51 -0400

    Initial Commit

---
+++ On The Block/On_The_BlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_The_BlockApp.swift
+//  On The Block
+//
+//  Created by Abe Molina on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct On_The_BlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
